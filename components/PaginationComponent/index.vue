f88a9f4a62f4078c870ae177a617de99444d506e
Vlad3221 <seregaduru3000@mail.ru>
2023-09-05 21:27:16 +0300

    Task_composible

--- components/PaginationComponent/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="article__navigation">
-    <button>Назад</button>
-    <button>Далее</button>
-  </div>
-</template>
-
-<script lang="ts" src="./index.ts"></script>
-<style lang="scss" src="./index.scss"></style>
